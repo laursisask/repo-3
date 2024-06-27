037fbfbf348671d01c9a23f1309fecadb2c45d37
Kelly Wallach <kelly.wallach@amplitude.com>
2024-06-27 12:38:03 -0400

    chore: remove coverage threshold from jest config

--- jest.config.js
+++ jest.config.js
@@ -9,14 +9,6 @@ module.exports = {
   collectCoverageFrom: ['**/src/**/*.ts'],
   coverageReporters: ['lcov', 'text-summary'],
   restoreMocks: true,
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   verbose: true,
   modulePathIgnorePatterns: ['<rootDir>/lib'],
 };
